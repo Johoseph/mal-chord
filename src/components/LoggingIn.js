c789b147c07c4b967961467ffb97c3e9861970e4
Joseph Moroney <josephmoroney97@gmail.com>
2021-08-07 02:18:58 +1000

    Better logging in

--- src/components/LoggingIn.js
+++ src/components/LoggingIn.js
@@ -1,17 +1,37 @@
 import { h } from "preact";
-import styled from "styled-components";
-import { SankeyLoading } from "./graph/loading/SankeyLoading";
+import styled, { keyframes } from "styled-components";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 const Wrapper = styled.div`
   height: calc(100vh - 100px);
   display: flex;
   align-items: center;
+  justify-content: center;
+`;
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Spinner = styled.div`
+  font-size: 6rem;
+  color: var(--accent);
+  & svg {
+    animation: ${rotate} 2s linear infinite;
+  }
 `;
 
 const LoggingIn = () => {
   return (
     <Wrapper>
-      <SankeyLoading hght={75} />
+      <Spinner>
+        <AiOutlineLoading3Quarters />
+      </Spinner>
     </Wrapper>
   );
 };
