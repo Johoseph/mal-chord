a03f5732ccb07c845ff20af9cea1da673dd7ce3f
Joseph Moroney <josephmoroney97@gmail.com>
2021-08-20 18:35:25 +1000

    Revert preact config tests

--- preact.config.js
+++ preact.config.js
@@ -1,8 +1,6 @@
-import path from "path";
 import envVars from "preact-cli-plugin-env-vars";
 
 export default function (config, env, helpers) {
-  config.resolve.alias.components = path.join(__dirname, "src/components");
   envVars(config, env, helpers);
   config.node.process = true;
   config.node.Buffer = true;
