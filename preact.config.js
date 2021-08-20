be45046be1470cafe7d75855e89e1b886a8b804c
Joseph Moroney <josephmoroney97@gmail.com>
2021-08-20 18:29:55 +1000

    Preact config test v2

--- preact.config.js
+++ preact.config.js
@@ -2,7 +2,7 @@ import path from "path";
 import envVars from "preact-cli-plugin-env-vars";
 
 export default function (config, env, helpers) {
-  config.resolve.alias.src = path.resolve(__dirname, "src");
+  config.resolve.alias.components = path.join(__dirname, "src/components");
   envVars(config, env, helpers);
   config.node.process = true;
   config.node.Buffer = true;
